088d68271c468f39e6b2193956a8447e7d590cb8
Ashfak Hossain <ashfaksikder15@gmail.com>
2023-12-10 22:07:48 +0600

    fix some style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,15 +32,15 @@ const Home = () => {
     });
   }, []);
   return (
-    <div className="flex full-calculator bg-white flex-col items-center justify-center h-[100vh]">
+    <div className="flex full-calculator overflow-hidden  bg-white flex-col items-center justify-center h-[100vh]">
       <h1 className="text-4xl text-black mb-5 font-bold font-i">Calculator</h1>
-      <section className="container relative w-[370px] overflow-hidden backdrop-blur-xl bg-white/80 rounded-2xl border z-50 ">
+      <section className="container relative w-[340px] md:w-[370px] overflow-hidden backdrop-blur-xl bg-white/80 rounded-2xl border z-50 ">
         <div className="calculator">
           <form>
             <div className="p-2 ">
               <input
                 ref={displayRef}
-                className="w-full text-right rounded-xl text-black h-[6.4rem] bg-transparent display focus:outline-0 duration-300 p-3 text-4xl"
+                className="w-full text-right rounded-xl text-black h-[6rem] md:h-[6.4rem] bg-transparent display focus:outline-0 duration-300 p-3 text-4xl"
                 type="text"
                 name="display"
                 id="display"
@@ -49,105 +49,105 @@ const Home = () => {
             <main className="">
               <div className="grid grid-cols-4 p-2 gap-3 justify-around ">
                 <input
-                  className="px-5 py-3  bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-red-400 rounded-lg "
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3  bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-red-400 rounded-lg "
                   type="button"
                   value="AC"
                 />
                 <input
-                  className="px-5 py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-cyan-500 rounded-lg "
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-cyan-500 rounded-lg "
                   type="button"
                   value="DE"
                 />
                 <input
-                  className="px-5 py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-cyan-500 rounded-lg"
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-cyan-500 rounded-lg"
                   type="button"
                   value="%"
                 />
                 <input
-                  className="px-5 py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-cyan-500 rounded-lg"
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-cyan-500 rounded-lg"
                   type="button"
                   value="/"
                 />
               </div>
               <div className="grid grid-cols-4 p-2 gap-3 justify-around">
                 <input
-                  className="px-5 py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3 bg-[#eee] text-[1.35rem]  md:text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
                   type="button"
                   value="7"
                 />
                 <input
-                  className="px-5 py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
                   type="button"
                   value="8"
                 />
                 <input
-                  className="px-5 py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
                   type="button"
                   value="9"
                 />
                 <input
-                  className="px-5 py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-cyan-500 rounded-lg"
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-cyan-500 rounded-lg"
                   type="button"
                   value="*"
                 />
               </div>
               <div className="grid grid-cols-4 p-2 gap-3 justify-around">
                 <input
-                  className="px-5 py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
                   type="button"
                   value="4"
                 />
                 <input
-                  className="px-5 py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
                   type="button"
                   value="5"
                 />
                 <input
-                  className="px-5 py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
                   type="button"
                   value="6"
                 />
                 <input
-                  className="px-5 py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-cyan-500 rounded-lg"
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-cyan-500 rounded-lg"
                   type="button"
                   value="-"
                 />
               </div>
               <div className="grid grid-cols-4 p-2 gap-3 justify-around">
                 <input
-                  className="px-5 py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
                   type="button"
                   value="1"
                 />
                 <input
-                  className="px-5 py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
                   type="button"
                   value="2"
                 />
                 <input
-                  className="px-5 py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
                   type="button"
                   value="3"
                 />
                 <input
-                  className="px-5 py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-cyan-500 rounded-lg"
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-cyan-500 rounded-lg"
                   type="button"
                   value="+"
                 />
               </div>
               <div className="grid grid-cols-4 p-2 gap-3 justify-around">
                 <input
-                  className="px-5 py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
                   type="button"
                   value="0"
                 />
                 <input
-                  className="px-5 py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-black rounded-lg"
                   type="button"
                   value="."
                 />
                 <input
-                  className="px-5 py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-cyan-500 rounded-lg col-span-2"
+                  className="px-[1.12rem] md:px-5 py-[0.70rem] md:py-3 bg-[#eee] text-2xl shadow-[3px_3px_10px_1px_#edf2f7] bg-opacity-90 text-cyan-500 rounded-lg col-span-2"
                   type="button"
                   value="="
                 />
