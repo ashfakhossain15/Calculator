fe03343fca9eafbecead730d830bcd36e2a89245
Ashfak Hossain <ashfaksikder15@gmail.com>
2023-12-10 21:49:13 +0600

    added all functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,16 @@ const Home = () => {
     let output = "";
 
     const calculate = (btnValue: any) => {
-      if(btnValue === "="&& btnValue !== "") {
+      if(btnValue === "="&& output !== "") {
         output = eval(output.replace("%", "/100"))
       }else if (btnValue === "AC"){
         output = "";
       }
       else if(btnValue === "DE"){
         output = output.toString().slice(0,-1)
+      }else{
+        if(output=== "" && specialChars.includes(btnValue)) return;
+        output += btnValue;
       }
       display.value = output;
 
