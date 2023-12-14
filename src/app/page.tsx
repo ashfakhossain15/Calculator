62a81bc909ade3686ef1b2c850ba6b4825613854
Ashfak Hossain <ashfaksikder15@gmail.com>
2023-12-14 23:29:21 +0600

    font had been changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ const Home = () => {
   }, []);
   return (
     <div className="flex full-calculator bg-white flex-col items-center justify-center h-[100vh]">
-      <h1 className="text-4xl text-black mb-5 font-bold font-i">Calculator</h1>
+      <h1 className="text-4xl text-black mb-5 font-bold">Calculator</h1>
       <section className="container relative w-[340px] md:w-[370px] overflow-hidden backdrop-blur-xl bg-white/80 rounded-2xl border z-50 ">
         <div className="calculator">
           <form>
